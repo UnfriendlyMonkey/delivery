3026ce5e55454ec523d8be55a64581f5a83c93b6
Andrey Sereda <andreysereda@ya.ru>
2025-09-06 21:23:09 +0300

    Module-2 HW. Add Location VE

---
+++ internal/core/domain/kernel/location.go
@@ -0,0 +1,90 @@
+// Package kernel for Value Objects common for all app
+package kernel
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+	"delivery/internal/pkg/errs"
+)
+
+const (
+	MinX = 1
+	MaxX = 10
+	MinY = 1
+	MaxY = 10
+)
+
+func NewLocation(x, y uint8) (Location, error) {
+	if x < MinX || x > MaxX {
+		return Location{}, errs.NewValueIsOutOfRangeError("x", x, MinX, MaxX)
+	}
+	if y < MinY || y > MaxY {
+		return Location{}, errs.NewValueIsOutOfRangeError("y", y, MinY, MaxY)
+	}
+	loc := Location{
+		x:     x,
+		y:     y,
+		valid: true,
+	}
+	return loc, nil
+}
+
+func MinLocation() Location {
+	loc, _ := NewLocation(1, 1)
+	return loc
+}
+
+func MaxLocation() Location {
+	loc, _ := NewLocation(10, 10)
+	return loc
+}
+
+func RandomLocation() (Location, error) {
+	randomizer := rand.New(rand.NewSource(time.Now().Unix()))
+	randx := randomizer.Intn(10) + 1
+	randy := randomizer.Intn(10) + 1
+	loc, err := NewLocation(uint8(randx), uint8(randy))
+	if err != nil {
+		return Location{}, err
+	}
+	return loc, nil
+}
+
+type Location struct {
+	x     uint8
+	y     uint8
+	valid bool
+}
+
+func (l Location) X() uint8 {
+	return l.x
+}
+
+func (l Location) Y() uint8 {
+	return l.y
+}
+
+func (l Location) Equal(target Location) bool {
+	return l == target
+}
+
+func (l Location) IsValid() bool {
+	return l.valid
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func (l Location) Distance(target Location) (uint8, error) {
+	if !target.IsValid() {
+		return 0, fmt.Errorf("target location: %v is not valid", target)
+	}
+	distX := abs(int(l.X()) - int(target.X()))
+	distY := abs(int(l.Y()) - int(target.Y()))
+	return uint8(distX) + uint8(distY), nil
+}
---
+++ internal/core/domain/kernel/location_test.go
@@ -0,0 +1,123 @@
+package kernel_test
+
+import (
+	"delivery/internal/core/domain/kernel"
+	"delivery/internal/pkg/errs"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewLocCorrectParams(t *testing.T) {
+	// Arrange
+
+	// Act
+	loc, err := kernel.NewLocation(3, 4)
+
+	// Assert
+	assert.NotEmpty(t, loc, "new location should not be empty")
+	assert.NoError(t, err, "should not be error creating new location")
+	assert.True(t, loc.IsValid(), "new location should be valid")
+	assert.Equal(t, uint8(3), loc.X(), "location X shall be 3")
+	assert.Equal(t, uint8(4), loc.Y(), "location Y shall be 4")
+}
+
+func Test_NewLocWithOutOfRangeReturnError(t *testing.T) {
+	// Arrange
+	tests := map[string]struct {
+		x        uint8
+		y        uint8
+		expected error
+	}{
+		"wrong_x": {
+			x: uint8(15),
+			y: uint8(4),
+			expected: errs.NewValueIsOutOfRangeError("x", uint8(15), kernel.MinX, kernel.MaxX),
+		},
+		"wrong_y": {
+			x: uint8(3),
+			y: uint8(0),
+			expected: errs.NewValueIsOutOfRangeError("y", uint8(0), kernel.MinY, kernel.MaxY),
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			// Act
+			_, err := kernel.NewLocation(test.x, test.y)
+
+			// Assert
+			if err.Error() != test.expected.Error() {
+				t.Errorf("expected %v, got %v", test.expected, err)
+			}
+		})
+	}
+}
+
+func TestRandomLocation(t *testing.T) {
+	for i := range 11 {
+		t.Run(fmt.Sprintf("test RandomLocation %d", i), func(t *testing.T) {
+			got, err := kernel.RandomLocation()
+			assert.NotEmpty(t, got, "random location shouldn't be empty")
+			assert.NoError(t, err, "shouln't be errors creating random location")
+		})
+	}
+}
+
+func TestMinLocation(t *testing.T) {
+	// Arrange
+	// Act
+	loc := kernel.MinLocation()
+
+	// Assert
+	assert.NotEmpty(t, loc, "new location should not be empty")
+	assert.Equal(t, loc.X(), uint8(kernel.MinX))
+	assert.Equal(t, loc.Y(), uint8(kernel.MinY))
+}
+
+func TestMaxLocation(t *testing.T) {
+	// Arrange
+	// Act
+	loc := kernel.MaxLocation()
+
+	// Assert
+	assert.NotEmpty(t, loc, "new location should not be empty")
+	assert.Equal(t, loc.X(), uint8(kernel.MaxX))
+	assert.Equal(t, loc.Y(), uint8(kernel.MaxY))
+}
+
+func TextLocationsEqual(t *testing.T) {
+	// Arrange
+	// Act
+	loc1 := kernel.MaxLocation()
+	loc2 := kernel.MaxLocation()
+
+	// Assert
+	assert.True(t, loc1.Equal(loc2), "two max locations should be equal")
+}
+
+func TextLocationsNotEqual(t *testing.T) {
+	// Arrange
+	// Act
+	loc1 := kernel.MaxLocation()
+	loc2 := kernel.MinLocation()
+
+	// Assert
+	assert.False(t, loc1.Equal(loc2), "min and max locations should not be equal")
+}
+
+func Test_MinMaxLocationsDistance(t *testing.T) {
+	// Arrange
+	// Act
+	loc1 := kernel.MaxLocation()
+	loc2 := kernel.MinLocation()
+	d1, err1 := loc1.Distance(loc2)
+	d2, err2 := loc2.Distance(loc1)
+	
+	// Assert
+	assert.Equal(t, uint8(d1), uint8(18), "distance between min and max should be 18")
+	assert.NoError(t, err1)
+	assert.NoError(t, err2)
+	assert.Equal(t, uint8(d1), uint8(d2), fmt.Sprintf("expected equal, got: %d != %d", d1, d2))
+}
